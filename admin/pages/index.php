5d1a10607b803d7cd193d88417b81333ab9e6b28
BrayanRL01 <brayan12345303@gmail.com>
2022-11-16 13:45:35 -0600

    Se eliminó código de index.php

--- admin/pages/index.php
+++ admin/pages/index.php
@@ -34,22 +34,6 @@
             </div>
         </form>
 
-        <?php
-        if (isset($_GET["error"])) {
-            switch ($_GET["error"]) {
-                case '1':
-                    echo "<p class='text-tertiary'>* No autorizado</p>";
-                    break;
-                case '2':
-                    echo "<p class='text-tertiary'>* Se requiere email y contraseña</p>";
-                    break;
-                case '3':
-                    echo "<p class='text-tertiary'>* Credenciales invalidos</p>";
-                    break;
-            }
-        }
-        ?>
-
     </section>
 </body>
 
@@ -71,32 +55,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <style>
-        .navbar {
-            margin-bottom: 50px;
-            border-radius: 0;
-        }
-
-        /* Remove the jumbotron's default bottom margin */
-        .jumbotron {
-            margin-bottom: 0;
-        }
-
-        /* Add a gray background color and some padding to the footer */
-        footer {
-            background-color: #f2f2f2;
-            padding: 25px;
-        }
-
-        .btn {
-            padding: 10px 20px;
-            background-color: #dd4b39;
-            color: #f1f1f1;
-            border-radius: 0;
-            transition: .2s;
-            margin-left: 120px;
-        }
-    </style>
     </head>
 
     <body>
